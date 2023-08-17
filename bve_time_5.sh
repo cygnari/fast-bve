8377afa1c700131f2b0387b085a19ccea220abd4
Anthony Chen <cygnari@berkeley.edu>
2023-08-17 09:11:25 +0900

    Stuff

--- bve_time_5.sh
+++ bve_time_5.sh
@@ -2,7 +2,7 @@
 #PBS -N bve_time_5
 #PBS -A UMIC0093
 #PBS -l walltime=12:00:00
-#PBS -q regular
+#PBS -q main
 #PBS -j oe
 #PBS -k eod
 #PBS -m abe
@@ -10,7 +10,7 @@
 #PBS -l select=1:ncpus=36:mpiprocs=36
 #PBS -l place=group=rack
 
-export TMPDIR=/glade/scratch/$USER/temp
+export TMPDIR=/glade/derecho/scratch/$USER/temp
 mkdir -p $TMPDIR
 
 mpirun -np 1 ./driver > $TMPDIR/run_out5.txt

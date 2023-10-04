f6bce63d4981f0f528809f3478fc4985312ad0e5
Anthony Chen <cygnari@berkeley.edu>
2023-10-04 15:47:02 -0600

    Expand tests

--- tests/general_util_tests.cpp
+++ tests/general_util_tests.cpp
@@ -12,5 +12,10 @@
 TEST(CountNanTest, BasicAssertions) {
   std::vector<double> test_vec{std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()};
   EXPECT_EQ(2, count_nans(test_vec));
+}
 
+TEST(DotProdTest, BasicAssertions) {
+  std::vector<double> vec1 {1, 2, 3};
+  std::vector<double> vec2 {-1, -2, 1};
+  EXPECT_EQ(-2, dot_prod(vec1, vec2));
 }

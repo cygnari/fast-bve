aca619b7bd55b3e09a007e035f60ab57b47ce663
Anthony Chen <cygnari@berkeley.edu>
2023-10-12 19:54:42 -0400

    Add relative l2 error calculation script

---
+++ job_scripts/pbs_bve_error.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N bve_run
+#PBS -A UMIC0093
+#PBS -l walltime=12:00:00
+#PBS -q main
+#PBS -j oe
+#PBS -k eod
+#PBS -m abe
+#PBS -M cygnari@umich.edu
+#PBS -l select=1:ncpus=128:mpiprocs=128
+#PBS -l place=group=rack
+
+export TMPDIR=/glade/derecho/scratch/$USER/temp
+mkdir -p $TMPDIR
+
+mpirun -np 128 ../build/executables/single_rhs_error_test > $TMPDIR/run_out.txt

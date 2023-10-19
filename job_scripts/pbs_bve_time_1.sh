e1817313818959c963bef6d7ab17a922c0980066
Anthony Chen <cygnari@berkeley.edu>
2023-10-19 18:03:51 -0400

    Update job scripts

--- job_scripts/pbs_bve_time_1.sh
+++ job_scripts/pbs_bve_time_1.sh
@@ -7,10 +7,10 @@
 #PBS -k eod
 #PBS -m abe
 #PBS -M cygnari@umich.edu
-#PBS -l select=4:ncpus=128:mpiprocs=128
+#PBS -l select=2:ncpus=128:mpiprocs=128
 #PBS -l place=group=rack
 
 export TMPDIR=/glade/derecho/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpiexec ../build/executables/single_rhs > $TMPDIR/run_out81.txt
+mpiexec ../build/executables/single_rhs > $TMPDIR/run_out71.txt

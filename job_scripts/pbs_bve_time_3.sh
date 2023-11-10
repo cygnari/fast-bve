1175d2107f7cedc12d42fa8ddfbb159f60aa8777
Anthony Chen <cygnari@berkeley.edu>
2023-11-10 13:56:36 -0500

    job scripts

--- job_scripts/pbs_bve_time_3.sh
+++ job_scripts/pbs_bve_time_3.sh
@@ -13,4 +13,4 @@
 export TMPDIR=/glade/derecho/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-mpiexec ../build/executables/single_rhs > $TMPDIR/run_out13.txt
+mpiexec ../build/executables/single_rhs > $TMPDIR/run_out23.txt
